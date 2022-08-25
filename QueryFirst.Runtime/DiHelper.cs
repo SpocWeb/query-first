11dc4a306eda6b6e08be3525026e66a55d5d3efc
Simon Boddy <sboddy@elcia.com>
2022-08-25 16:21:59 +0200

    Move DIHelper

---
+++ QueryFirst.Runtime/DiHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace QueryFirst
+{
+    public class DiHelper
+    {
+        public void RegisterAllQueries(IServiceCollection services, Assembly assembly)
+        {
+            var QfRepos = assembly
+                .GetTypes()
+                .Where(t => t.Name.EndsWith("QfRepo") && t.IsClass)
+                .OrderBy(t => t.Name)
+                .ToList();
+            var QfInterfaces = assembly
+                .GetTypes()
+                .Where(t => t.Name.EndsWith("QfRepo") && t.IsInterface)
+                .OrderBy(t => t.Name.Substring(1))
+                .ToList();
+            if (QfRepos.Count() != QfInterfaces.Count())
+                throw new ApplicationException("Mismatch between classes and interfaces ending QfRepo.");
+            for (int i = 0; i < QfRepos.Count(); i++)
+            {
+                services.Add(new ServiceDescriptor(
+                    QfInterfaces[i],
+                    QfRepos[i],
+                    ServiceLifetime.Singleton
+                ));
+            }
+        }
+    }
+}
